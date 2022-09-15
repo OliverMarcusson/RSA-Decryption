02bc9eaf89894b149557bcfb64c3ecbb52c6e899
Oliver Marcusson <oliver.marcusson@elev.ga.ntig.se>
2022-09-15 17:51:25 +0200

    Initial intended functionality complete

--- main.py
+++ main.py
@@ -26,59 +26,55 @@ def is_prime(number):
     else:
         return False
 
-def find_enc():
+def get_keypair(F):
     while True:
         encryption_key = random.randint(1, F)
         if is_prime(encryption_key) == True:
-            return encryption_key
-
-def find_decryption(encryption_key):
-    i = 1
-    while True:
-        if (i * encryption_key) % F == 1:
-            return i
-        else:
-            i += 1
-
-def main():
-       
-    prime_numbers = find_primes(100)
-    p = prime_numbers[-1]
-    q = prime_numbers[-2]
-    n = p*q
-    F = (p-1)*(q-1)
-
+            break
     
-
-    print(f"p: {p}\nq: {q}\nn: {n}\nF: {F}\nENC key: {encryption_key}")
-
-
-    decryption_key = find_decryption()
-
-    message = 69
-    decrypted = (encrypted ** decryption_key) % n
-
-    print(f"\nOriginal Message: {message}")
-    print(f"Encrypted message: {encrypted}")
-    print(f"Decrypted message: {decrypted}")
+    decryption_key = 1
+    while True:
+        if (decryption_key * encryption_key) % F == 1:
+            return encryption_key, decryption_key
+        else:
+            decryption_key += 1
  
 print('What do you want to do?\n1. Encrypt\n2. Decrypt')
 option = int(input(':'))
 
 if option == 1:
-    message = int(input('Please input a message to encrypt:'))
+    print('Do you have an encryption key?\n1. Yes\n2. No')
+    has_keypair = int(input(':'))
+    
+    if has_keypair == 1:
+        encryption_key = int(input('\nPlease enter encryption key: '))
+    
+    message = int(input('Please input a message to encrypt: '))
     prime_numbers = find_primes(100)
     p = prime_numbers[-1]
     q = prime_numbers[-2]
     n = p*q
     F = (p-1)*(q-1)
-    encryption_key = find_enc()
-    encrypted = (message ** encryption_key) % n
     
-    print(f"Done!\nEncrypted message: {encrypted}\nEncryption key: {encryption_key}")
+    if has_keypair != 1:
+        encryption_key, decryption_key = get_keypair(F)
+    
+    encrypted_message = (message ** encryption_key) % n
+    
+    if has_keypair != 1:
+        print(f"\nDone!\nEncrypted message: {encrypted_message}\n\n[ Keypair ]\nEncryption:{encryption_key}\nDecryption:{decryption_key}")
+    else:
+        print(f"\nDone!\nEncrypted message: {encrypted_message}\n\n[ Key ]\nEncryption:{encryption_key}")
 
 
 
-else: 
-    encrypted_message = int(input('Please input a message to decrypt:'))
+elif option == 2: 
+    encrypted_message = int(input('Please input a message to decrypt: '))
+    decryption_key = int(input('Please input your decryption key: '))
+    prime_numbers = find_primes(100)
+    p = prime_numbers[-1]
+    q = prime_numbers[-2]
+    n = p*q
+    decrypted_message = (encrypted_message ** decryption_key) % n
+    print(f"\nDone!\nDecrypted message: {decrypted_message}\n\n[ Key ]\nDecryption:{decryption_key}")
 
